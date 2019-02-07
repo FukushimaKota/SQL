f60e83b4f994b92d76cff18d451892a59e0c0b10
福島　Ｋ <kota@fukushimanoMacBook-Pro.local>
2019-02-07 10:49:53 +0900

    Q9まで終了

--- Q9.sql
+++ Q9.sql
@@ -1,1 +1,1 @@
-SELECT 　FROM item_category INNER JOIN  item on  WHERE total_price;
+SELECT category_name,SUM(item_price) AS total_price FROM item_category INNER JOIN  item on item_category.category_id = item.category_id GROUP BY item_category.category_id  ORDER BY total_price DESC;
